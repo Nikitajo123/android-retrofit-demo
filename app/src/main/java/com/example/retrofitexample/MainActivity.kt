57a7af5a34df727d21af736f0b3a47b94da13b9a
Nikita Jovanov <nikita.jovanov@student.um.si>
2025-12-10 17:29:16 +0100

    Lastna uporaba in prikaz osnovne funkcionalnosti

--- app/src/main/java/com/example/retrofitexample/MainActivity.kt
+++ app/src/main/java/com/example/retrofitexample/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.retrofitexample
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        val btnLoad = findViewById<Button>(R.id.btnLoad)
+
+        btnLoad.setOnClickListener {
+            lifecycleScope.launch {
+                try {
+                    val response = RetrofitInstance.api.getPost()
+
+                    if (response.isSuccessful) {
+                        val post = response.body()
+                        txtResult.text = "ID: ${post?.id}\nTitle: ${post?.title}"
+                    } else {
+                        txtResult.text = "Server error: ${response.code()}"
+                    }
+
+                } catch (e: Exception) {
+                    txtResult.text = "Exception occurred: ${e.localizedMessage}"
+                }
+            }
         }
     }
 }
